7477a7c794691924d8725624c6263ff9fa0b579e
Filip <filsva@edu.umea.se>
2021-06-08 09:31:41 +0200

    .

--- js/monke.js
+++ js/monke.js
@@ -1,1 +1,38 @@
-let greg = document.getElementsByClassName("greg")
+//På grund av dålig struktur, dålig motivation, lathet, och sen start på projektet, medföljde även strull med främst JS
+//Nedan finns kod som var ett desperat försök till att JS skulle fungera, dock gick inget riktigt ihop
+
+/*
+let monkeButtCrib = document.getElementById("roosterSquare")
+let gregVar = document.getElementById("Greg");
+
+gregVar.addEventListener("click", function ButtVisGreg(){
+    this.style.opacity = 1;
+}, true)
+gregVar.addEventListener("mouseout", function ButtClearGreg(){
+    this.id = "hejelton"
+}, true)
+
+
+
+//Kod från https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame, 
+//mest bara för att använda mig av requestAnimationFrame
+const element = document.getElementById("startText");
+let start;
+
+function step(timestamp) {
+  if (start === undefined)
+    start = timestamp;
+  const elapsed = timestamp - start;
+
+  // `Math.min()` is used here to make sure that the element stops at exactly 200px.
+  element.style.transform = 'translateX(' + Math.min(0.1 * elapsed, 200) + 'px)';
+
+  if (elapsed < 2000) { // Stop the animation after 2 seconds
+    window.requestAnimationFrame(step);
+  }
+}
+
+window.requestAnimationFrame(step(2)); 
+*/
+
+
